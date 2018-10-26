2581a5e0a8bb67385a30d6c83b21e8866cf31a42
sverholen <sverholen@gmail.com>
2018-10-26 14:12:47 +0200

    Javadoc

--- src/main/java/be/vdab/goededoel/entities/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/entities/GoedeDoel.java
@@ -2,15 +2,32 @@ package be.vdab.goededoel.entities;
 
 import java.math.BigDecimal;
 
+/**
+ * Een <strong> goed doel waarvoor men cash inzamelt
+ * @author Stijn.Verholen
+ *
+ */
 public class GoedeDoel {
 	private final String naam;
 	private BigDecimal opgebracht = BigDecimal.ZERO;
+	/**
+	 * Instantiates a GoedeDoel object.
+	 * @param naam the charity's name.
+	 */
 	public GoedeDoel(String naam) {
 		this.naam = naam;
 	}
+	/**
+	 * Returns the charity's name
+	 * @return the charity's name
+	 */
 	public String getNaam() {
 		return naam;
 	}
+	/**
+	 * Return the amount that was donated.
+	 * @return the amount that was donated.
+	 */
 	public BigDecimal getOpgebracht() {
 		return opgebracht;
 	}
---
+++ src/main/java/be/vdab/goededoel/entities/package-info.java
@@ -0,0 +1,4 @@
+/**
+* The classes in this package represent things in the real world
+*/
+package be.vdab.goededoel.entities;
