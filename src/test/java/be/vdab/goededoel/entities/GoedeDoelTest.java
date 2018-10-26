d2b73fca8f36d5b4062644cc535b2043d57c28f2
sverholen <sverholen@gmail.com>
2018-10-26 13:54:58 +0200

    Fixed code coverage

--- src/test/java/be/vdab/goededoel/entities/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/entities/GoedeDoelTest.java
@@ -1,6 +1,7 @@
 package be.vdab.goededoel.entities;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import java.math.BigDecimal;
 
@@ -25,5 +26,15 @@ public class GoedeDoelTest {
 	@Test
 	public void aNewCharityHasNotEarned() {
 		assertEquals(0, doel.getOpgebracht().compareTo(BigDecimal.ZERO));
+	}
+	
+	@Test
+	public void goedeDoelenMetDezelfdeNaamMoetenVolgensEqualsGelijkZijn() {
+	assertEquals(new GoedeDoel("CLINICLOWNS"), doel);
+	}
+	@Test
+	public void goedeDoelenMetVerschillendeNaamMoetenVolgensEqualsVerschillendZijn()
+	{
+	assertNotEquals(new GoedeDoel("Unicef"), doel);
 	}
 }
